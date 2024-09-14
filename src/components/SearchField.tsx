90dd21cdf5a10712966762924e4d265803f64efb
salmanrancho <shareefsalman600@gmail.com>
2024-09-15 01:09:00 +0530

    updated search feature +(rewrites)

--- src/components/SearchField.tsx
+++ src/components/SearchField.tsx
@@ -1,22 +1,22 @@
 "use client";
 
+import { SearchIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { Input } from "./ui/input";
-import { SearchIcon } from "lucide-react";
 
 export default function SearchField() {
   const router = useRouter();
 
-  function handSubmit(e: React.FormEvent<HTMLFormElement>) {
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     const form = e.currentTarget;
     const q = (form.q as HTMLInputElement).value.trim();
     if (!q) return;
-    router.push(`/Search?q=${encodeURIComponent(q)}`);
+    router.push(`/search?q=${encodeURIComponent(q)}`);
   }
 
   return (
-    <form onSubmit={handSubmit} method="GET" action="/search">
+    <form onSubmit={handleSubmit} method="GET" action="/search">
       <div className="relative">
         <Input name="q" placeholder="Search" className="pe-10" />
         <SearchIcon className="absolute right-3 top-1/2 size-5 -translate-y-1/2 transform text-muted-foreground" />
